ba327bdaaabe6d1507b6d385991a2dc63101d64b
Marie <mje.espinosa@gmail.com>
2025-09-01 15:56:56 +0200

    add header to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { TodoListComponent } from './features/todos/components/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, TodoListComponent],
+  template: `
+    <app-header></app-header>
+    <main class="container mx-auto p-4">
+      <app-todo-list></app-todo-list>
+    </main>
+  `,
+  styles: [],
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'todo-list-app';
 }
