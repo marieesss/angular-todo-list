b54f7451aae653dba1fbfc43acf1cd93eb7f9281
Marie <mje.espinosa@gmail.com>
2025-09-02 09:36:15 +0200

    fix header

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-
 export const AUTH_ROUTES: Routes = [
   {
     path: 'login',
     component: LoginComponent,
   },
+  {
+    path: 'register',
+    // TODO: Ajouter RegisterComponent dans la Partie 2
+    redirectTo: '/todos',
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
 ];
-
-console.log('AUTH_ROUTES');
